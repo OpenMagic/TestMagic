0ab6993db2c1f7d063459b540476834568362b07
Tim Murphy <tim@26tp.com>
2013-07-28 08:06:35 +1000

    Setup standard projects, etc.

---
+++ Projects/TestMagic/GWT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TestMagic
+{
+    public class GWT
+    {
+        public static GWT Given<T>(T givenValue)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GWT When(Action action)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GWT Then<TException>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GWT ShouldBeThrown()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GWT ForParameter(string paramName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-TestMagic
-=========
+# TestMagic
 
 Collection of test helpers.
+
+NOTE: This library cannot use any of the *Magic packages because in the future they will depend on this library.
