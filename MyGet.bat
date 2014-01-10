9c251affcef1fcc55dff7c906879941f7a386e08
Tim Murphy <tim@26tp.com>
2014-01-10 15:33:45 +1100

    Fixed MyGet.bat for myget.org environment.

--- MyGet.bat
+++ MyGet.bat
@@ -1,13 +1,88 @@
 @echo off
 
-msbuild /p:Configuration=Release
+echo Setting up build environment...
+echo -------------------------------
+echo.
+
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+if "%PackageVersion%" == "" (
+
+  rem Simulate myget.org environment.
+  set PackageVersion=999.99
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+echo Building solution...
+echo -------------------------------
+echo.
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild /p:Configuration=Release
+if not "%errorlevel%" == "0" goto Error
 echo.
 echo.
 
-mstest /TestContainer:Projects\TestMagic.Tests\bin\Release\TestMagic.Tests.dll
+echo Running unit tests...
+echo -------------------------------
+echo.
+
+if "%GallioEcho%" == "" (
+
+  rem Simulate myget.org environment.
+  "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\mstest.exe" /testcontainer:Projects\TestMagic.Tests\bin\Release\TestMagic.Tests.dll"
+) else (
+
+  rem myget.org build server.
+  %GallioEcho%  Projects\TestMagic.Tests\bin\Release\TestMagic.Tests.dll
+)
+
+if not "%errorlevel%" == "0" goto Error
 echo.
 echo.
 
-pushd .nuget
-nuget pack TestMagic.nuspec -Version %PackageVersion%
-popd
+echo Building NuGet package...
+echo -------------------------
+echo.
+
+if exist .\Build\nul (
+
+  echo .\Build folder exists.
+  
+) else (
+
+  echo Creating .\Build folder...
+  md .\Build
+)
+
+echo.
+echo Creating NuGet package...
+
+.\.nuget\nuget pack .\.nuget\TestMagic.nuspec -o .\Build %Version%
+
+if not "%errorlevel%" == "0" goto Error
+echo.
+echo.
+
+:Success
+echo.
+echo.
+echo Build was successful.
+echo =====================
+exit 0
+
+:Error
+echo.
+echo.
+echo **************************
+echo *** An error occurred. ***
+echo **************************
+echo.
+echo.
+exit -1
--- Scripts/TestMyGetBuild.bat
+++ Scripts/TestMyGetBuild.bat
@@ -1,16 +1,5 @@
 @echo off
 
-rem Fake the MyGet build server environment.
-
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
-set PackageVersion=999.999
-
-rem Do the MyGet build
-
 pushd ..\
-call MyGet.bat
+call MyGet.bat > Scripts\Build.log
 popd 
-
-echo.
-echo.
-pause
