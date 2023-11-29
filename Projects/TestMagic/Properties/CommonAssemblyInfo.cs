42c08083593db70db640fa903ed2d24037a63786
Tim Murphy <tim@26tp.com>
2023-11-29 15:55:48 +1100

    Upgrade to .net framework 4.8 (#1)
    * Update to .NET Framework 4.8
    
    * Update Fody.NullGuard NuGuard NuGet package so solution will compile
    
    * Add ./.vs/ folder to git ignore list
    
    * Fix AssemblyInfo of all projects

---
+++ Projects/TestMagic/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyProduct("OpenMagic")]
+[assembly: AssemblyCompany("Tim Murphy")]
+[assembly: AssemblyCopyright("Copyright © Tim Murphy 2013-2022")]
+[assembly: CLSCompliant(false)]
+
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
